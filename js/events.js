6db13558b1c9b44683003485ebc52eeffe7cb70e
Becczs <rebecca.anderssoncarlsson@gmail.com>
2025-02-24 15:37:20 +0100

    js

--- js/events.js
+++ js/events.js
@@ -200,10 +200,9 @@ allBtn.addEventListener('click', () => filterEvents('all'))
 
 
 if (document.getElementById("logoutButton")) {
-    document.getElementById("logoutButton").addEventListener("click", (event) => {
-      event.preventDefault(); 
-      sessionStorage.removeItem("currentUser");
-      window.location.href = "login.html"; 
-    });
-  }
-  
+  document.getElementById("logoutButton").addEventListener("click", (event) => {
+    event.preventDefault();
+    sessionStorage.removeItem("currentUser");
+    window.location.href = "login.html";
+  });
+}
